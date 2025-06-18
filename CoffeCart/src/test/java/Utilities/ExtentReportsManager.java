1c04de6d3481ce17c264969e425ede3a34a7b62d
RB-QA-Automation <rajabhamra95@gmail.com>
2025-06-18 21:09:03 +0100

    Latest Push - Data Driven, Single .xml testing

--- CoffeCart/src/test/java/Utilities/ExtentReportsManager.java
+++ CoffeCart/src/test/java/Utilities/ExtentReportsManager.java
@@ -1,6 +1,7 @@
 package Utilities;
 
 import com.aventstack.extentreports.ExtentReports;
+
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
