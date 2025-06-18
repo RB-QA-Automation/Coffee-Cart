355823ba01ef3d39cd6f5c6dac3f846f900441c5
RB-QA-Automation <rajabhamra95@gmail.com>
2025-06-18 19:48:05 +0100

    Managing Utils

--- CoffeCart/src/test/java/Utilities/ExtentTestNGListener.java
+++ CoffeCart/src/test/java/Utilities/ExtentTestNGListener.java
@@ -11,7 +11,6 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 
-import Utils.ExtentReportsManager;
 import base.BaseTest;
 
 public class ExtentTestNGListener implements ITestListener {
